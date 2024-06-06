3e17b8b186c88a86e4287ffa510e14433746ea15
Nayak-Sahil <nayaksahil992003@gmail.com>
2024-06-06 15:42:35 +0530

    Add: Splash & Dashboard Screen

--- app/src/main/java/com/kotharigroup/kotharipipes/MainActivity.java
+++ app/src/main/java/com/kotharigroup/kotharipipes/MainActivity.java
@@ -13,8 +13,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /** EdgeToEdge can make your app display edge-to-edge—using the entire width and height of the display—by
+         *  drawing behind the system bars.
+         *  The system bars are the {status bar and the navigation bar}.
+        */
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
+
+        // To apply padding so that view can't overlap in systembars.
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
---
+++ app/src/main/java/com/kotharigroup/kotharipipes/Splash.java
@@ -0,0 +1,45 @@
+package com.kotharigroup.kotharipipes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        View decorView = getWindow().getDecorView();
+        // following settings just hide the navigation bar
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+
+        // To hide both {navigation bar & status bar} use following:
+        // int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        // moving to MainActivity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+    }
+}
