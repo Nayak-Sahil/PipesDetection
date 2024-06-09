793e58ad7f4f35a25cfcdf2fc2e0e09cefe4a84f
Nayak-Sahil <nayaksahil992003@gmail.com>
2024-06-09 15:46:07 +0530

    Add: Detection Activity

--- app/src/main/java/com/kotharigroup/kotharipipes/MainActivity.java
+++ app/src/main/java/com/kotharigroup/kotharipipes/MainActivity.java
@@ -1,8 +1,25 @@
 package com.kotharigroup.kotharipipes;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.ContextMenu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,6 +27,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton optionBtn;
+    Button viewHistoryBtn, takePhotoBtn, chooseImgBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,5 +46,42 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        optionBtn = findViewById(R.id.optionBtn);
+
+        viewHistoryBtn = findViewById(R.id.viewHistoryBtn);
+        takePhotoBtn = findViewById(R.id.takePhotoBtn);
+        chooseImgBtn = findViewById(R.id.chooseImageBtn);
+
+        optionBtn.setOnCreateContextMenuListener(this);
+
+        ActivityResultLauncher<Intent> launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult o) {
+                if (o.getResultCode() == Activity.RESULT_OK) {
+                    Intent data = o.getData();
+                    Bitmap img = (Bitmap) data.getExtras().get("data");
+                    Intent detectionIntent = new Intent(getApplicationContext(), PipesDetection.class);
+                    detectionIntent.putExtra("pipes_img", img);
+                    startActivity(detectionIntent);
+                }
+            }
+        });
+
+        takePhotoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraI = new Intent();
+                cameraI.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                launcher.launch(cameraI);
+            }
+        });
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.context_menu, menu);
+        super.onCreateContextMenu(menu, v, menuInfo);
     }
 }
---
+++ app/src/main/java/com/kotharigroup/kotharipipes/PipesDetection.java
@@ -0,0 +1,31 @@
+package com.kotharigroup.kotharipipes;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PipesDetection extends AppCompatActivity {
+
+    ImageView imgView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_pipes_detection);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        imgView = findViewById(R.id.imgView);
+        Bitmap img = getIntent().getParcelableExtra("pipes_img");
+        imgView.setImageBitmap(img);
+    }
+}
